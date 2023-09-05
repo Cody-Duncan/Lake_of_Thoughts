e9da9633c069ca0a0d6c5ace9633b300da4f49dc
Cody Duncan <cody.h.duncan@live.com>
2023-09-05 00:25:54 -0700

    Add dependency for node assert.

--- src/site/_data/eleventyComputed.js
+++ src/site/_data/eleventyComputed.js
@@ -1,5 +1,6 @@
 const { getGraph } = require("../../helpers/linkUtils");
 const { getFileTree } = require("../../helpers/filetreeUtils");
+const { assert } = require('node:assert/strict');
 const { userComputed } = require("../../helpers/userUtils");
 
 module.exports = {
